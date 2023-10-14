b078d54ea9d845862d65a3796afef6ec8ea934c8
Diego Lélis <diegollelis@gmail.com>
2023-10-14 00:40:40 -0300

    Projeto To-Do List do curso online de Java da Rocketseat.

---
+++ src/test/java/br/com/diegolelis/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diegolelis.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
